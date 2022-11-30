06bb6fef62a7776440f76f678af52924c9eb3784
yuanf225 <admin@itpel.com>
2022-11-30 16:47:28 +0800

    Update AC_2022_08_15_00.sql

--- sql/updates/world/AC_2022_08_15_00.sql
+++ sql/updates/world/AC_2022_08_15_00.sql
@@ -1,3 +1,3 @@
 DELETE FROM `acore_string` WHERE `entry`=30091;
 INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES 
-(30091, '|cffffff00[|cffff0000反作弊警报|r|cffffff00]:|r 战场启动传送漏洞检测|cFFFF8C00[|Hplayer:%s|h%s|h|r|cFFFF8C00] - Latency: %u ms', NULL, NULL, NULL, NULL, NULL, '', '', NULL);
+(30091, '|cffffff00[|cffff0000反作弊警报|r|cffffff00]:|r 战场启动传送漏洞检测|cFFFF8C00[|H玩家：%s|h%s|h|r|cFFFF8C00] - 延迟: %u ms', NULL, NULL, NULL, NULL, NULL, '', '', NULL);
